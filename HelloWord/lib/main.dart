02bf209bcd93440e253924c85a1151a7c8f69174
dju4mota <fernando.b@ges.inatel.com>
2021-02-12 11:16:24 -0300

    Navigation and Saving Names

--- HelloWord/lib/main.dart
+++ HelloWord/lib/main.dart
@@ -3,5 +3,18 @@ import 'ExternalPackage.dart';
 import 'HelloWorld.dart';
 import 'ListView.dart';
 import 'StatefulWidget.dart';
+import 'NameList.dart';
 
-void main() => runApp(RandomListView());
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // #docregion build
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      home: NameList(),
+    );
+  }
+  // #enddocregion build
+}
